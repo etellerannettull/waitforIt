c3cfe8cc0d94173b0777a646061ea5e6a8b13c4c
Trude <trude.martinsen@gmail.com>
2012-07-02 21:11:36 +0200

    Oppgave 7 impl

--- SteriaOpen/src/oppgaver/Oppgave7.java
+++ SteriaOpen/src/oppgaver/Oppgave7.java
@@ -2,4 +2,23 @@ package oppgaver;
 
 public class Oppgave7 {
 
+	public String finnPalindrom(String tekst) {
+		int venstre = 0;
+		int høyre = tekst.length() - 1;
+
+		int antall = 0;
+
+		while (venstre < høyre) {
+			if (tekst.charAt(venstre) != tekst.charAt(høyre)) {
+				break;
+			}
+
+			venstre++;
+			høyre--;
+			antall++;
+		}
+
+		return antall == tekst.length() / 2 ? "PALINDROM-TOM" : "" + antall;
+	}
+
 }
---
+++ SteriaOpen/test/oppgaver/Oppgave7Test.java
@@ -0,0 +1,58 @@
+package oppgaver;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Oppgave7Test {
+
+	@Test
+	public void skal_returnere_palindrom_tom() {
+		Oppgave7 opg = new Oppgave7();
+		String palindrom = opg.finnPalindrom("12344321");
+		assertEquals("PALINDROM-TOM", palindrom);
+	}
+
+	@Test
+	public void skal_finne_antall_tall_som_er_palindrom_for_12321() {
+		Oppgave7 opg = new Oppgave7();
+		String palindrom = opg.finnPalindrom("123421");
+		assertEquals("2", palindrom);
+	}
+
+	@Test
+	public void skal_finne_antall_tall_som_er_palindrom_for_12345321() {
+		Oppgave7 opg = new Oppgave7();
+		String palindrom = opg.finnPalindrom("12345321");
+		assertEquals("3", palindrom);
+	}
+
+	@Test
+	public void skal_finne_antall_tall_som_er_palindrom_for_12531() {
+		Oppgave7 opg = new Oppgave7();
+		String palindrom = opg.finnPalindrom("12531");
+		assertEquals("1", palindrom);
+	}
+
+	@Test
+	public void skal_finne_antall_tall_som_er_palindrom_for_12521() {
+		Oppgave7 opg = new Oppgave7();
+		String palindrom = opg.finnPalindrom("12521");
+		assertEquals("PALINDROM-TOM", palindrom);
+	}
+
+	@Test
+	public void skal_finne_antall_tall_som_er_palindrom_for_41587514() {
+		Oppgave7 opg = new Oppgave7();
+		String palindrom = opg.finnPalindrom("41587514");
+		assertEquals("3", palindrom);
+	}
+
+	@Test
+	public void skal_finne_antall_tall_som_er_palindrom_for_97144179() {
+		Oppgave7 opg = new Oppgave7();
+		String palindrom = opg.finnPalindrom("97144179");
+		assertEquals("PALINDROM-TOM", palindrom);
+	}
+
+}
