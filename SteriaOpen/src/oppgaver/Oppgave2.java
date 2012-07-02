8b6437273e8619737cf57dca4ad047baec7482c2
Magnus Westergaard <westerga@gmail.com>
2012-07-02 22:28:59 +0200

    Rask impl oppgave 2

--- SteriaOpen/src/oppgaver/Oppgave2.java
+++ SteriaOpen/src/oppgaver/Oppgave2.java
@@ -1,13 +1,39 @@
 package oppgaver;
 
 import java.util.Map;
+import java.util.Map.Entry;
 
 public class Oppgave2 {
 
 	private static final String KJELL = "Kjell";
 
 	public String finnKjell(Map<String, String> kart) {
-		return "OMGGIRLZ";
+		boolean kjellFinnes = false;
+		for (Entry<String, String> sjefenTil : kart.entrySet()) {
+			kjellFinnes |= sjefenTil.getKey().equals(KJELL) || sjefenTil.getValue().equals(KJELL);
+		}
+		if(!kjellFinnes) {
+			return "OMGGIRLZ";
+		} else {
+		
+		String sjefenTil = KJELL;
+		int stegOpp = 0;
+		while(finnSjefenTil(sjefenTil, kart) != null) {
+			stegOpp++;
+			sjefenTil = finnSjefenTil(sjefenTil, kart);
+		}
+		
+		return "" + stegOpp;
+		}
+	}
+	
+	private String finnSjefenTil(String navn, Map<String, String> kart) {
+		for (Entry<String, String> sjefenTil : kart.entrySet()) {
+			if(sjefenTil.getValue().equals(navn)) {
+				return sjefenTil.getKey();
+			}
+		}
+		return null;
 	}
 
 }
--- SteriaOpen/test/oppgaver/Oppgave2Test.java
+++ SteriaOpen/test/oppgaver/Oppgave2Test.java
@@ -5,7 +5,6 @@ import static org.junit.Assert.assertEquals;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.junit.Ignore;
 import org.junit.Test;
 
 public class Oppgave2Test {
@@ -18,14 +17,24 @@ public class Oppgave2Test {
 	}
 
 	@Test
-	@Ignore
-	public void skal_finne_kjell_på_topp() throws Exception {
+	public void skal_finne_kjell_på_topp() {
 		Map<String, String> kart = new HashMap<>();
 		kart.put("Kjell", "Gunnar");
 
 		Oppgave2 opg = new Oppgave2();
 		String svar = opg.finnKjell(kart);
 		assertEquals("0", svar);
+	}
+
+	@Test
+	public void skal_finne_kjell_midt_i() {
+		Map<String, String> kart = new HashMap<>();
+		kart.put("Kjell", "Gunnar");
+		kart.put("Trond", "Kjell");
+		
+		Oppgave2 opg = new Oppgave2();
+		String svar = opg.finnKjell(kart);
+		assertEquals("1", svar);
 	}
 
 }
