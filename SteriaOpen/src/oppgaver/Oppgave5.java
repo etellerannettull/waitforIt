adc4408312b118ba36e45c0c365409d479a68b54
Trude <trude.martinsen@gmail.com>
2012-07-02 20:18:07 +0200

    Oppgave 5 implementasjon

--- SteriaOpen/src/oppgaver/Oppgave5.java
+++ SteriaOpen/src/oppgaver/Oppgave5.java
@@ -1,5 +1,25 @@
 package oppgaver;
 
+// ROT13, Zed Shaw
 public class Oppgave5 {
 
+	public String finnRot13(String tekst) {
+		char[] nyeBokstaver = new char[tekst.length()];
+		for (int i = 0; i < tekst.length(); i++) {
+			char c = tekst.charAt(i);
+			int nyPlassering = (int) c;
+
+			if (nyPlassering >= (int) 'A' && nyPlassering <= (int) 'z') {
+				nyPlassering = (int) c + 13;
+
+				if ((Character.isLowerCase(c) && nyPlassering > (int) 'z')
+						|| (Character.isUpperCase(c) && nyPlassering > (int) 'Z')) {
+					nyPlassering = nyPlassering - 26;
+				}
+			}
+			nyeBokstaver[i] = (char) nyPlassering;
+		}
+
+		return new String(nyeBokstaver);
+	}
 }
---
+++ SteriaOpen/test/oppgaver/Oppgave5Test.java
@@ -0,0 +1,86 @@
+package oppgaver;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Oppgave5Test {
+
+	@Test
+	public void skal_finne_rot13_av_a() {
+		Oppgave5 opg = new Oppgave5();
+		String rot13 = opg.finnRot13("a");
+		assertEquals("n", rot13);
+	}
+
+	@Test
+	public void skal_finne_rot13_av_b() {
+		Oppgave5 opg = new Oppgave5();
+		String rot13 = opg.finnRot13("b");
+		assertEquals("o", rot13);
+	}
+
+	@Test
+	public void skal_finne_rot13_av_ab() {
+		Oppgave5 opg = new Oppgave5();
+		String rot13 = opg.finnRot13("ab");
+		assertEquals("no", rot13);
+	}
+
+	@Test
+	public void skal_finne_rot13_av_n() {
+		Oppgave5 opg = new Oppgave5();
+		String rot13 = opg.finnRot13("n");
+		assertEquals("a", rot13);
+	}
+
+	@Test
+	public void skal_finne_rot13_av_A() {
+		Oppgave5 opg = new Oppgave5();
+		String rot13 = opg.finnRot13("A");
+		assertEquals("N", rot13);
+	}
+
+	@Test
+	public void skal_finne_rot13_av_N() {
+		Oppgave5 opg = new Oppgave5();
+		String rot13 = opg.finnRot13("N");
+		assertEquals("A", rot13);
+	}
+
+	@Test
+	public void skal_finne_rot13_av_AnN() {
+		Oppgave5 opg = new Oppgave5();
+		String rot13 = opg.finnRot13("AnN");
+		assertEquals("NaA", rot13);
+	}
+
+	@Test
+	public void skal_finne_rot13_av_en_setning_med_komma_og_utropstegn() {
+		Oppgave5 opg = new Oppgave5();
+		String rot13 = opg.finnRot13("Hei, du!");
+		assertEquals("Urv, qh!", rot13);
+	}
+
+	@Test
+	public void skal_finne_rot13_av_programming_motherfucker() {
+		Oppgave5 opg = new Oppgave5();
+		String rot13 = opg.finnRot13("Programming, motherfucker");
+		assertEquals("Cebtenzzvat, zbgureshpxre", rot13);
+	}
+
+	@Test
+	public void skal_finne_rot13_av_steria_open() {
+		Oppgave5 opg = new Oppgave5();
+		String rot13 = opg.finnRot13("Steria Open");
+		assertEquals("Fgrevn Bcra", rot13);
+	}
+
+	@Test
+	public void skal_finne_rot13_av_zed_shaw() {
+		Oppgave5 opg = new Oppgave5();
+		String rot13 = opg.finnRot13("Zed Shaw");
+		assertEquals("Mrq Funj", rot13);
+	}
+
+}
