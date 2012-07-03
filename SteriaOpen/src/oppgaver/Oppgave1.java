bc5c740bb5696ef3426e54b99287141d126dff89
Trude <trude.martinsen@gmail.com>
2012-07-03 20:57:16 +0200

    Oppgave 1- formatert en dato..jej for java date..

--- SteriaOpen/src/oppgaver/Oppgave1.java
+++ SteriaOpen/src/oppgaver/Oppgave1.java
@@ -1,5 +1,45 @@
 package oppgaver;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Oppgave1 {
+
+	public String formaterDato(Date date) {
+		SimpleDateFormat sf = new SimpleDateFormat("dd.MM.YYYY HH:mm");
+		return sf.format(date);
+	}
+
+	// Clojureløsning fra Bodil: :)
+	// (rule-ints #".*leaves from Paris (\d+)\.(\d+)\.(\d+) at (\d+):(\d+)\. It
+	// takes (\d+) minutes.*"
+	// [y mo d h m duration]
+	// (let [startmins (+ (* h 60) m)]
+	// (let [endmins (+ startmins duration)]
+	// (str y "." mo "." d " at " (padzero (int (/ endmins 60))) ":" (padzero
+	// (mod endmins 60))))))
+	//
+	// (rule-ints #".*leaves (\d+)\.(\d+)\.(\d+) at (\d+):(\d+)\. It takes (\d+)
+	// minutes.*"
+	// [y mo d h m duration]
+	// (let [startmins (+ (* h 60) m)]
+	// (let [endmins (+ startmins (+ duration 60))]
+	// (str y "." mo "." d " at " (padzero (int (/ endmins 60))) ":" (padzero
+	// (mod endmins 60))))))
+	// (rule-ints #".*Rome takes off (\d+)\.(\d+)\.(\d+) at (\d+):(\d+)\. It
+	// takes (\d+) minutes.*"
+	// [y mo d h m duration]
+	// (let [startmins (+ (* h 60) m)]
+	// (let [endmins (+ startmins duration)]
+	// (str y "." mo "." d " at " (padzero (int (/ endmins 60))) ":" (padzero
+	// (mod endmins 60))))))
+	//
+	// (rule-ints #".*New Your takes off (\d+)\.(\d+)\.(\d+) at (\d+):(\d+)\. It
+	// takes (\d+) minutes.*"
+	// [y mo d h m duration]
+	// (let [startmins (+ (* h 60) m)]
+	// (let [endmins (+ startmins (+ 360 duration))]
+	// (str y "." mo "." d " at " (padzero (int (/ endmins 60))) ":" (padzero
+	// (mod endmins 60))))))
 
 }
--- SteriaOpen/test/oppgaver/Oppgave1Test.java
+++ SteriaOpen/test/oppgaver/Oppgave1Test.java
@@ -2,13 +2,33 @@ package oppgaver;
 
 //import static org.junit.Assert.*;
 
+import static org.junit.Assert.assertEquals;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.Locale;
+import java.util.TimeZone;
+
 import org.junit.Test;
 
 public class Oppgave1Test {
 
 	@Test
-	public void test() {
-		//fail("Not yet implemented");
+	public void skal_skjønne_hvordan_calendar_fønker() {
+		Calendar cal = new GregorianCalendar(TimeZone.getTimeZone("GMT+01:00"));
+		cal.set(2012, 10, 30, 3, 2, 3);
+		SimpleDateFormat sf = new SimpleDateFormat("dd.MM.YYYY HH:mm", Locale.FRENCH);
+		System.out.println((sf.format(cal.getTime())));
 	}
 
+	@Test
+	public void skal_returnere_innsendt_dato_på_riktig_format() {
+		Oppgave1 opg = new Oppgave1();
+		Calendar cal = new GregorianCalendar(TimeZone.getTimeZone("GMT+01:00"));
+		cal.set(2012, 10, 30, 3, 0, 0); // måned er 0-basert, så 10 er
+										// november........bah...
+		String dato = opg.formaterDato(cal.getTime());
+		assertEquals("30.11.2012 03:00", dato);
+	}
 }
