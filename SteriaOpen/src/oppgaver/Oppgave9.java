69b42fb6d91c1dad10652f356fdf20f42b0377ae
Trude <trude.martinsen@gmail.com>
2012-08-14 10:31:18 +0200

    Oppgave9 - lagt til test

--- SteriaOpen/src/oppgaver/Oppgave9.java
+++ SteriaOpen/src/oppgaver/Oppgave9.java
@@ -2,4 +2,21 @@ package oppgaver;
 
 public class Oppgave9 {
 
+	private int antallEtasjer;
+	private int lavesteEtasje;
+	private int høyesteEtasje;
+
+	public void setAntallEtasjer(int antallEtasjer) {
+		this.antallEtasjer = antallEtasjer;
+	}
+
+	public void setEtasjeRangeHvorDøreneÅpnes(int lavesteEtasje, int høyesteEtasje) {
+		this.lavesteEtasje = lavesteEtasje;
+		this.høyesteEtasje = høyesteEtasje;
+	}
+
+	public int finnAntallTrekk(int i, int j, int k, int l, int m) {
+		return 0;
+	}
+
 }
---
+++ SteriaOpen/test/oppgaver/Oppgave9Test.java
@@ -0,0 +1,17 @@
+package oppgaver;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class Oppgave9Test {
+
+	@Test
+	public void skal_finne_antall_trekk_for_testsett() {
+		Oppgave9 opg = new Oppgave9();
+		opg.setAntallEtasjer(49);
+		opg.setEtasjeRangeHvorDøreneÅpnes(21, 25);
+		assertEquals(opg.finnAntallTrekk(17, 26, 20, 19, 31), 8);
+	}
+
+}
